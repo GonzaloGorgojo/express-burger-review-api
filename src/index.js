0b802add37a0d92b20c6aefe70168580391f92fd
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-03-27 13:34:07 -0300

    Enable cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const burgerRoute = require("./routes/burgers");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(burgerRoute);
 app.use(express.static("public"));
